56214849d47205be2eddbcdfdd622aa38e1c6458
Vitor Martins Cruz <vitormartinscruz.dev@gmail.com>
2021-01-14 23:39:11 -0300

    Adding main, webscrapping, settings etc

--- src/core.py
+++ src/core.py
@@ -1,2 +1,120 @@
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater, Dispatcher
+from telegram import Bot, Update
+import os
 from conf.settings import TELEGRAM_TOKEN
+import json
+
+
+def read_json():
+    with open('/home/vitormrts/Área de Trabalho/Projects/corona-virus-telegram-bot/src/ranking.json', 'r', encoding='utf8') as f:
+        return json.load(f)
+
+
+def total_cases(bot, update: Update, **optional_args):
+    data = read_json()
+    data_cases = data['totcases']
+    i = 0
+    msg = ''
+    for case in data_cases:
+        if i == 0:
+            msg += f"{case['total']} total active cases in World 🤒🌎\n\n"
+        else:
+            msg += f"{i}) {case['country']}\nTotal cases: {case['total']}\n\n"
+        i += 1
+    bot.message.reply_text(msg, quote=False)
+
+
+def new_cases(bot, update: Update):
+    data = read_json()
+    data_cases = data['newcases']
+    i = 0
+    msg = ''
+    for case in data_cases:
+        if i == 0:
+            msg += f"{case['total']} total active cases in World 🤒🌎\n\n"
+        else:
+            msg += f"{i}) {case['country']}\nTotal cases: {case['total']}\n\n"
+        i += 1
+
+    bot.message.reply_text(msg, quote=False)
+
+
+def total_deaths(bot, update: Update):
+    data = read_json()
+    data_cases = data['totdeaths']
+    msg = ''
+    i = 0
+    for case in data_cases:
+        if i == 0:
+            msg += f"{case['total']} total active cases in World 🤒🌎\n\n"
+        else:
+            msg += f"{i}) {case['country']}\nTotal cases: {case['total']}\n\n"
+        i += 1
+
+    bot.message.reply_text(msg, quote=False)
+
+
+def new_deaths(bot, update: Update):
+    data = read_json()
+    data_cases = data['newdeaths']
+    i = 0
+    msg = ''
+    for case in data_cases:
+        if i == 0:
+            msg += f"{case['total']} total active cases in World 🤒🌎\n\n"
+        else:
+            msg += f"{i}) {case['country']}\nTotal cases: {case['total']}\n\n"
+        i += 1
+    bot.message.reply_text(msg, quote=False)
+
+
+def total_recovered(bot, update: Update):
+    data = read_json()
+    data_cases = data['totrecovered']
+    i = 0
+    msg = ''
+    for case in data_cases:
+        if i == 0:
+            msg += f"{case['total']} total active cases in World 🤒🌎\n\n"
+        else:
+            msg += f"{i}) {case['country']}\nTotal cases: {case['total']}\n\n"
+        i += 1
+
+    bot.message.reply_text(msg, quote=False)
+
+
+def active_cases(bot, update: Update):
+    data = read_json()
+    data_cases = data['activecases']
+    msg = ''
+    i = 0
+    for case in data_cases:
+        if i == 0:
+            msg += f"{case['total']} total active cases in World 🤒🌎\n\n"
+        else:
+            msg += f"{i}) {case['country']}\nTotal cases: {case['total']}\n\n"
+        i += 1
+
+    bot.message.reply_text(msg, quote=False)
+
+
+def webhook():
+    updater = Updater(token=TELEGRAM_TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('totalcases', total_cases))
+    dispatcher.add_handler(CommandHandler('newcases', new_cases))
+    dispatcher.add_handler(CommandHandler('totaldeaths', total_deaths))
+    dispatcher.add_handler(CommandHandler('newdeaths', new_deaths))
+    dispatcher.add_handler(CommandHandler('totalrecovered', total_recovered))
+    dispatcher.add_handler(CommandHandler('activecases', active_cases))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    print("press CTRL + C to cancel.")
+    webhook()
---
+++ src/webscrapping_data.py
@@ -0,0 +1,64 @@
+import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import json
+import simplejson
+
+top = {}
+rankings = {
+    'totcases': {'aria-label': 'TotalCases'},
+    'newcases': {'aria-label': 'NewCases', },
+    'totdeaths': {'aria-label': 'TotalDeaths'},
+    'newdeaths': {'aria-label': 'NewDeaths'},
+    'totrecovered': {'aria-label': 'TotalRecovered'},
+    'activecases': {'aria-label': 'ActiveCases'},
+}
+
+
+def create_rank(type):
+    arialabel = rankings[type]['aria-label']
+
+    if arialabel != 'TotalCases':
+        browse.find_element_by_xpath(
+            f"//table[@id='main_table_countries_today']//thead//tr//th[@aria-label='{arialabel}: activate to sort column descending']").click()
+
+    element = browse.find_element_by_xpath(
+        "//table[@id='main_table_countries_today']")
+    html_content = element.get_attribute('outerHTML')
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    table = soup.find(name='table')
+
+    df_full = pd.read_html(str(table))[0].head(11)
+    df = df_full[['#', 'Country,Other', f'{arialabel}']]
+    df.columns = ['pos', 'country', 'total']
+    df.dropna(axis='columns')
+
+    print(df)
+
+    return df.to_dict('records')
+
+
+url = "https://www.worldometers.info/coronavirus/#countries"
+
+option = Options()
+option.headless = True  # firefox invisibel to us (action in background)
+browse = webdriver.Firefox(options=option)
+
+browse.get(url)
+browse.maximize_window()
+time.sleep(10)
+
+for rank in rankings:
+    top[rank] = create_rank(rank)
+
+browse.quit()
+
+with open('./src/ranking.json', 'w', encoding='utf8') as jp:
+    #js = json.dumps(top, indent=4)
+    js = simplejson.dumps(top, ignore_nan=True, indent=4)
+
+    jp.write(js)
